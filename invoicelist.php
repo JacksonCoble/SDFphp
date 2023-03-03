f141fe4f426382011e3be57175c788a891ac97b2
jacksoncoble <0677247@schoolsnet.act.edu.au>
2023-03-03 15:08:43 +1100

    Navbar and csv websire img custom changes

---
+++ invoicelist.php
@@ -0,0 +1,26 @@
+<?php include "template.php" ?>
+    <title>Invoice List</title>
+    <body>
+
+<?php
+// Read the contents of the file
+$currentRow = 0;
+if (($handle = fopen("orders.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $numberOfRowsOfData = count($data);
+        $currentRow++; //Add one to the current row
+
+// Customer Details
+        $cusNameFirst = $data[0];
+        $cusNameSecond = $data[1];
+        echo "<p><a href='invoice.php?invoiceNumber=".$currentRow."'>".$cusNameFirst." ".$cusNameSecond."</a></p>";
+
+
+
+    }
+    fclose($handle);    //Closes the File
+
+}
+?>
+
+<a href="URL">LINK TEXT</a>
